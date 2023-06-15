c54a68db774dfe3beda7ddcfabd5a742f9a1899d
Bram Peirs <bram.peirs@showpad.com>
2023-06-15 18:14:30 +0200

    tailwind

--- src/main.ts
+++ src/main.ts
@@ -54,16 +54,14 @@ function updateTable() {
         labelStatus = "label-success";
       }
       tableBody +=
-        "<tr>" +
-        "<th>" +
+        "<tr class='bg-white border-b dark:bg-gray-800 dark:border-gray-700'>" +
+        "<th scope='row' class='px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white'>" +
         (i + 1) +
         "</th>" +
-        "<td>" +
+        "<td  class='px-6 py-4'>" +
         item.name +
         "</td>" +
-        '<td><span class="label ' +
-        labelStatus +
-        '">' +
+        "<td class='px-6 py-4'>" +
         item.availablecapacity +
         "</span></td>" +
         "</tr>";
